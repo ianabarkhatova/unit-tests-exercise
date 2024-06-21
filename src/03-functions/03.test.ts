e1670df09b30530ef799b1e205f997a977fd044d
Iana Barkhatova <yanabarhat@gmail.com>
2024-06-21 16:43:05 +0300

    write active and city tests for student

--- src/03-functions/03.test.ts
+++ src/03-functions/03.test.ts
@@ -1,5 +1,5 @@
 import {StudentType} from "../02-objects/02";
-import {addSkill} from "./03";
+import {addSkill, makeStudentActive, StudentLivesIn} from "./03";
 
 let student: StudentType;
 beforeEach(() => {
@@ -32,10 +32,29 @@ beforeEach(() => {
     }
 })
 
-test("new teach skill should be added to student", ()=> {
+test("new teach skill should be added to student", () => {
+
     addSkill(student, "JS");
 
     expect(student.technologies.length).toBe(4);
     expect(student.technologies[3].title).toBe("JS");
     expect(student.technologies[3].id).toBeDefined();
 })
+
+test("student should be made active", () => {
+
+    makeStudentActive(student)
+    expect(student.isActive).toBe(true);
+})
+
+test("does student live in city?", () => {
+
+    let result1 = StudentLivesIn(student, "Moscow")
+    let result2 = StudentLivesIn(student, "Minsk")
+    expect(result1).toBe(false);
+    expect(result2).toBe(true);
+
+})
+
+
+
--- src/03-functions/03.ts
+++ src/03-functions/03.ts
@@ -1,13 +1,17 @@
 import {StudentType} from "../02-objects/02";
 
 
-// debugger
-// export const sum = (a: number, b: number) => {
-    //what function returns:
-//     return(a + b);
-// }
-//
-// const result = sum(sum(1,2), sum(1,3));
+export const sum = (a: number, b: number) => {
+    // what function returns:
+    return (a + b);
+}
+
+export function sum2(a: number, b: number) {
+    // what function returns:
+    return (a + b);
+}
+
+const result = sum(sum(1, 2), sum(1, 3));
 
 
 export const addSkill = (student: StudentType, skill: string) => {
@@ -16,3 +20,19 @@ export const addSkill = (student: StudentType, skill: string) => {
         title: skill
     })
 }
+
+export function addSkill2(student: StudentType, skill: string) {
+    student.technologies.push({
+        id: new Date().getTime(),
+        title: skill
+    })
+}
+
+export const makeStudentActive = (s: StudentType) => {
+    s.isActive = true;
+}
+
+export function StudentLivesIn(s: StudentType, cityName: string) {
+    return s.address.city.title === cityName;
+}
+
