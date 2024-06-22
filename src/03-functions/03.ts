f3886e097468dbc20204a79cfb6ce9016d63cf4c
Iana Barkhatova <yanabarhat@gmail.com>
2024-06-22 10:46:34 +0300

    create tests for city

--- src/03-functions/03.ts
+++ src/03-functions/03.ts
@@ -1,4 +1,5 @@
 import {StudentType} from "../02-objects/02";
+import {CityType, governmentBuildingType, HouseType} from "../02-objects/02_02";
 
 
 export const sum = (a: number, b: number) => {
@@ -35,4 +36,27 @@ export const makeStudentActive = (s: StudentType) => {
 export function StudentLivesIn(s: StudentType, cityName: string) {
     return s.address.city.title === cityName;
 }
+
+export const changeBudget = (t: governmentBuildingType, num: number) => {
+    t.budget += num;
+}
+
+export const repairHouse = (h: HouseType) => {
+    h.repaired = true;
+}
+
+export const toFireStaff = (b: governmentBuildingType, num: number) => {
+    b.staffCount -= num;
+}
+
+export function toHireStaff(b: governmentBuildingType, num: number) {
+    b.staffCount += num;
+}
+
+export const createMessage = (props: CityType) => {
+    //шаблонные строки
+    return `Hello ${props.title}! All ${props.citizensNumber} people`;
+
+}
+
 
